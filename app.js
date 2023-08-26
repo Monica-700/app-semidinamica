10f739ccd3c84924f815f017f91dd5692e0ac639
Monica-700 <monicaeducate@gmail.com>
2023-08-26 18:38:32 -0500

    cv02

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hola Nodejs');
+//importar os paquetes como el express para  crear un web server en un entorno local.
+const express = require("express");
+const app = express();
+const port = 9595;
+
+//mostrar contenido de la carpeta public
+app.use(express.static("public"));
+
+//consultar las páginas en el webserver see utiliza el get, una función de flecha
+app.get("/left-sidebar", (req, res) => {
+  //res s utiliza para responder a la petición del usuario
+  res.sendFile(__dirname + "/public/left-sidebar.html");
+})
+app.get("/right-sidebar", (req, res) => {
+  //res s utiliza para responder a la petición del usuario
+  res.sendFile(__dirname + "/public/right-sidebar.html");
+})
+app.get("/no-sidebar", (req, res) => {
+  //res s utiliza para responder a la petición del usuario
+  res.sendFile(__dirname + "/public/no-sidebar.html");
+})
+app.get('*', (req, res) => {
+    //el comodín o asterisco sirve para validar la búsqueda de una página
+  //res s utiliza para responder a la petición del usuario
+  res.sendFile(__dirname + "/public/404.html");
+})
